2bd772ef86b2d43098e687d240d78b0ab6324be1
ITICLASSROOM\dlockwood024548 <dlockwood225@gmail.com>
2020-12-15 20:00:42 -0600

    Initial Commit

---
+++ SQL Tester Solution/SQL Tester Project/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace SQL_Tester_Project
+{
+    public partial class frmSQLTester : Form
+    {
+        SqlConnection booksConnection;
+        public frmSQLTester()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSQLTester_Load(object sender, EventArgs e)
+        {
+            booksConnection = new SqlConnection("Server=(localdb)\\MSSQLLocalDB;"
+                                               + "AttachDbFilename=" + Path.GetFullPath("SQLBooksDB.mdf")
+                                               + ";Integrated Security=True;"
+                                               + "Connect Timeout=30;");
+        }
+
+        private void frmSQLTester_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            booksConnection.Close();
+            booksConnection.Dispose();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            SqlCommand resultsCommand = null;
+            SqlDataAdapter resultsAdapter = new SqlDataAdapter();
+            DataTable resultsTable = new DataTable();
+            try
+            {
+                resultsCommand = new SqlCommand(txtSQLTester.Text, booksConnection);
+                resultsAdapter.SelectCommand = resultsCommand;
+                resultsAdapter.Fill(resultsTable);
+                grdSQLTester.DataSource = resultsTable;
+                lblRecords.Text = resultsTable.Rows.Count.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error in Processing SQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            resultsCommand.Dispose();
+            resultsAdapter.Dispose();
+            resultsTable.Dispose();
+        }
+    }
+}
